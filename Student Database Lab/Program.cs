44e1f385d02390962712e003c5d118ecebccb4c5
Vaxaldan <evankendall68@gmail.com>
2024-08-26 17:40:15 -0400

    lab update

---
+++ Student Database Lab/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Student_Database_Lab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studentName = { "Evan", "Jordan", "Ethan" };
+            string[] homeTown = { "Moses Lake", "Ogden", "T or C" };
+            string[] favFood = { "Pizza", "Hot dogs", "Green Chilles" };
+
+            Console.WriteLine("Which student number would you like to know more about?");
+            int studentNumber = int.Parse(Console.ReadLine());
+
+
+
+        }
+    }
+}
